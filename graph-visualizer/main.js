a1e280d87609bfd7ce315fad15f85a3304cd09ee
Ellora Devulapally <ellora.devulapally@duke.edu>
2026-03-23 14:04:52 -0400

    changes to main.js

--- graph-visualizer/main.js
+++ graph-visualizer/main.js
@@ -16,34 +16,92 @@ async function loadData() {
 }
 
 //Store data into global object
-IR_data = loadData().catch(console.warn);
+// IR_data = loadData().catch(console.warn);
 
 /**
  * Sets up the visualization, creating an SVG and reading in nodes
  * and edges
  */
-function initVis() {
-    const height = 300;
-    const width = 700;
-    const margin = {left: 10, top: 10, right: 10, bottom: 10};
+async function initVis() {
+    const data = await loadData();
+    const width = 200;
 
-    var svg = d3.select('#chart-area').append("svg")
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom);
+    const boxHeight = 200;
+    const svg = d3.select("#chart-area")
+        .append("svg")
+        .attr('viewBox', [0, 0, width, boxHeight]);
 
-    const nodes = IR_data.nodes.map(d => ({...d}));
+    // Define the arrowhead marker variables
+    const markerBoxWidth = 20;
+    const markerBoxHeight = 20;
+    const refX = markerBoxWidth / 2;
+    const refY = markerBoxHeight / 2;
+    const markerWidth = markerBoxWidth / 2;
+    const markerHeight = markerBoxHeight / 2;
+    const arrowPoints = [[0, 0], [0, 20], [20, 10]];
 
-    var edges = new Map();
+    // Loading in a constant for 
+    const nodes = data.nodes;
+    const edges = new Map();
 
+    console.log("nodes:", nodes);
+   
     nodes.forEach(node => {
         edges.set(node.id, node.edges)
         console.log(edges.get(node.id))
     });
 
+    var circles = []
+    var xCoordinate = 20;
+    var yCoordinate = 10;
+    nodes.forEach(node => {
+        node_coordinates = new Map
+        if (xCoordinate >= width) {
+            xCoordinate = 20;
+            yCoordinate += 10;
+        }
+        node_coordinates.set("x", xCoordinate);
+        node_coordinates.set("y", yCoordinate);
+        circles.push(node_coordinates)
+        xCoordinate += 10;
+        });
+
+    console.log("circles:", circles)
+
+    console.log(circles[0]);
+
+    svg.selectAll("circle")
+        .data(circles)
+        .enter()
+        .append("circle")
+        .attr("cx", (d, i) => circles[i].get("x"))  
+        .attr("cy", (d, i) => circles[i].get("y")) 
+        .attr("r", 1) 
+        .attr("fill", "steelblue");
+
+    svg.selectAll("edges")
+        .data(edges)
+        .enter()
+        .append(d3.linkHorizontal()
+        .x(d, circles[edges.get(i)])
+    )
+
+
+    // svg.selectAll("circle")
+    //     .data(nodes)
+    //     .enter()
+    //     .append("circle")
+    //     .attr("cx", (d, i) => (i * 40) % width + 20)  
+    //     .attr("cy", (d, i) => Math.floor(i / (width / 40)) * 40 + 20)
+    //     .attr("r", 5)
+    //     .attr("fill", "steelblue")
+    //     .attr("opacity", 0.7);
+
+
 }
 
-//Delay initVis to ensure data has loaded
-setTimeout(initVis, 2500);
 
+//Delay initVis to ensure data has loaded
+initVis();
 
 
